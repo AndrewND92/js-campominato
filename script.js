519ce6032d5b96b08c87c7c1379dca465ef1dd58
Andrea <miranda.andrea.nd@gmail.com>
2020-07-01 18:14:37 +0200

    tolti i duplicati

--- script.js
+++ script.js
@@ -2,29 +2,40 @@
 var numRandomPc = [];
 var numUtente = [];
 
-
 // genera 16 numeri casuali tra 1 e 100
-for (var i = 0; i < 16; i++) {
-  num = generatoreNum();
-  numRandomPc.push(num);
-}
+// I numeri non possono essere duplicati
 
-// chiedere all'utente tot volte un numero da 1 a 100
-for (var i = 0; i < 84; i++) {
+while( numRandomPc.length < 16 ) {
+  var num = generatoreNum();
+  var isIncluded = numRandomPc.includes(num);
 
-  var promptUtente = parseInt(prompt("insersci un numero tra 1 e 100"));
-  numUtente.push(promptUtente);
-
-  // controllo se ci sono dei numeri in comune nei due array
-  if (numRandomPc.includes(numUtente[i])) {
-    console.log("hey");
-    break;
+  if( isIncluded == false ) {
+  		numRandomPc.push(num);
   }
 }
+console.log(numRandomPc);
 
+// for (var i = 0; i < 16; i++) {
+//   num = generatoreNum();
+//   numRandomPc.push(num);
+// }
+
+// chiedere all'utente tot volte un numero da 1 a 100
+// L’utente non può inserire più volte lo stesso numero.
+// for (var i = 0; i < 84; i++) {
+//
+//   var promptUtente = parseInt(prompt("insersci un numero tra 1 e 100"));
+//   numUtente.push(promptUtente);
+//
+//   // controllo se ci sono dei numeri in comune nei due array
+//   if (numRandomPc.includes(numUtente[i])) {
+//     console.log("hey" + );
+//     break;
+//   }
+// }
 
 // FUNZIONE
- function generatoreNum(){
-   var n = Math.floor(Math.random() * 100)+1;
-   return n;
- }
+function generatoreNum() {
+  var n = Math.floor(Math.random() * 100)+1;
+  return n;
+}
